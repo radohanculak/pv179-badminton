28555ed03e1b48815e8ce0dbb16cdfd4a70b8125
adammydla <514329@mail.muni.cz>
2022-09-28 22:57:56 +0200

    Create DbContext

--- Sprint/Sprint.DAL/Data/DbContext.cs
+++ Sprint/Sprint.DAL/Data/DbContext.cs
@@ -3,10 +3,32 @@ using Sprint.DAL.Models;
 
 namespace Sprint.DAL.Data
 {
-    public class DbContext
+    public class SprintDbContext : DbContext
     {
-        public DbSet<User> User { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<Court> Courts { get; set; }
+        public DbSet<CourtReservation> CourtReservations { get; set; }
+        public DbSet<TrainerReservation> TrainerReservations { get; set; }
+        public DbSet<TrainerReview> TrainerReviews { get; set; }
+        public DbSet<TrainerPhoto> TrainerPhotos { get; set; }
 
-        //TODO
+        private string connectionString;
+
+        public SprintDbContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Change Sql server
+            // optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seeder();
+        }
     }
 }
