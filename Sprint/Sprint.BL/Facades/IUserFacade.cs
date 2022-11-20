8c50daa5eccce27566e53c7f72ca56f8bd1f52f7
adammydla <514329@mail.muni.cz>
2022-11-20 01:23:12 +0100

    Add Trainer facade

---
+++ Sprint/Sprint.BL/Facades/IUserFacade.cs
@@ -0,0 +1,10 @@
+using Sprint.BL.Dto.Trainer;
+using Sprint.BL.Dto.User;
+
+namespace Sprint.BL.Facades;
+
+public interface IUserFacade
+{
+    public Task<(UserDto, TrainerDto)> AddTrainerAsync(string firstName, string lastName,
+        string email, DateTime dateOfBirth, string description, decimal hourlyRate);
+}
